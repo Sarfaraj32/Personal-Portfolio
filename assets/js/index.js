f05cdc73feedab0d66432f67b30a16bf3738aa6f
Md Sarfaraj Ahmed <sarfaraj.holistify@gmail.com>
2025-06-17 14:18:20 +0530

    Create style switcher add style it and interact it fully

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,3 +7,44 @@ navToggle.addEventListener("click", () => {
   navMenu.classList.toggle("show-menu");
   navToggle.classList.toggle("animate-toggle");
 });
+
+// style switcher functionality
+
+const styleSwitcher = document.getElementById("style-switcher");
+switcherToggle = document.getElementById("switcher-toggle");
+switcherClose = document.getElementById("switcher-close");
+
+switcherToggle.addEventListener("click", () => {
+  styleSwitcher.classList.add("show-switcher");
+});
+
+switcherClose.addEventListener("click", () => {
+  styleSwitcher.classList.remove("show-switcher");
+});
+
+// theme switcher functionality
+
+const colors = document.querySelectorAll(".style-switcher-color");
+
+colors.forEach((color) => {
+  color.onclick = () => {
+    const activeColor = color.style.getPropertyValue("--hue");
+
+    colors.forEach((c) => c.classList.remove("active-color"));
+    color.classList.add("active-color");
+
+    document.documentElement.style.setProperty("--hue", activeColor);
+  };
+});
+
+// dark mode switcher functionality
+
+let currentTheme = "light";
+document.body.className = currentTheme;
+
+document.querySelectorAll('input[name="body-theme"]').forEach((input) => {
+  input.addEventListener("change", () => {
+    currentTheme = input.value;
+    document.body.className = currentTheme;
+  });
+});
